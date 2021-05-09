633d5499a007f1c4d61878e136c81f0c7b06d8cb
Mehdi <80045406+mehdij87@users.noreply.github.com>
2021-05-10 00:06:52 +0300

    First App by AJS

---
+++ app.js
@@ -0,0 +1,19 @@
+(function () {
+  "use strict";
+  angular.module("nameC", []).controller("nameCCal", function ($scope) {
+    $scope.name = "";
+    $scope.total = 0;
+    $scope.displayCal = function () {
+      let totalNamveValue = calculateNumericForString($scope.name);
+      $scope.total = totalNamveValue;
+    };
+
+    function calculateNumericForString(string) {
+      let totalStringValue = 0;
+      for (let i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
+    }
+  });
+})();
